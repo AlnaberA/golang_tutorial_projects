9e60bfc312de4e086bca40eba512b95bb6eead5d
Alaa Al Naber <alnaberalaa@gmail.com>
2019-04-13 20:56:50 -0400

    updating point.go

--- pointers/point.go
+++ pointers/point.go
@@ -5,8 +5,18 @@ import (
 )
 
 func main() {
-	num := 1
+	num1 := 0
+	num2 := 100
 
-	pointer := &num //Points to num
+	pointer := &num1 //Points to num1
 	fmt.Println(*pointer)
+	fmt.Println(pointer)
+
+	*pointer = 1 //Set value of pointer to 2 through the pointer.
+	fmt.Println(*pointer)
+	fmt.Println(pointer)
+
+	pointer = &num2
+	fmt.Println(*pointer)
+	fmt.Println(pointer)
 }
